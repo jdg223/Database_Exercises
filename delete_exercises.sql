12c612e089c4e23a68382b22c24a63e800346add
J.D. Garza <jdgarza22@yahoo.com>
2015-03-30 14:26:01 -0500

    now can be deleted

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,4 +1,4 @@
 USE codeup_test_db;
-DELETE * FROM albums WHERE Release_year > 1991;
-DELETE * FROM albums WHERE Genre = 'disco';
-DELETE * FROM albums WHERE Artist = 'Whitney Houston'; 
+SELECT * FROM albums WHERE Release_year > 1991;
+SELECT * FROM albums WHERE Genre = 'disco';
+SELECT * FROM albums WHERE Artist = 'Whitney Houston'; 
