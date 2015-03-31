91fb6c10bdcfdb8068d4340acad1163a0bb1c782
J.D. Garza <jdgarza22@yahoo.com>
2015-03-31 10:29:01 -0500

    sql queries for employees database

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,3 +3,8 @@ SELECT last_name FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND  '1999-12-31';
 SELECT * FROM employees WHERE hire_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT first_name FROM employees WHERE first_name IN ('Irena','Vidya','Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%E';
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; 
